b43b723256a32bc6974b46a3ced37c8268ffaf82
Muruga <davanon2014@gmail.com>
2021-06-15 11:58:54 +0530

    First

---
+++ src/main/java/com/iqmsoft/client/test.java
@@ -0,0 +1,10 @@
+package com.iqmsoft.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class test implements EntryPoint {
+  public void onModuleLoad() {
+
+				        				        
+  }
+}
